917afe7f2fd5720ba71af9d15d003a45316c0761
Po Han Chao <r03449013@gmail.com>
2022-03-13 20:40:11 +0800

    add loop

--- products.py
+++ products.py
@@ -9,18 +9,20 @@ while True:
 	price = input('The price of products: ')
 	p =[name, price]  #比較簡潔的寫法
 	products.append(p)
+#簡潔的寫法:
+#products.append([name, price])
 	
 #基礎寫法	
 #   p=[]
 #	p.append(name)
 #	p.append(price)
-   
-	
-#更加簡潔的寫法:
-#products.append([name, price])
-
 
 print(products)
 
 #存取二維清單
 #product[0][0] 先走進去第0格 
+
+for p in products:
+	print('The price of', p[0], 'is', p[1])
+
+
